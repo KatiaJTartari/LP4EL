c528c740dcca6fd2c7ff748970b1320475ba8781
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2022-01-12 17:38:42 -0300

    Add files via upload

---
+++ Aula9Exercicios1e2/HospedagemOnline/Helpers/HelperFrasePublicitaria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospedagemOnline.Helpers
+{
+    public static class HelperFrasePublicitaria
+    {
+        public static MvcHtmlString ExibeFrasePublicitaria(this HtmlHelper hp)
+        {
+            string str = "<h2> Viajar é investimento, escolher o local para se hospedar é necessidade </h2>";
+
+            return new MvcHtmlString(str);
+        }
+    }
+}
